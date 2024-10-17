d474d592a574d561a2fa9a4c0ec59b2e57601d94
dntruong81 <dntruong81@gmail.com>
2024-10-17 14:19:05 +0700

    Commit lan 2

--- FirstNullSafety.dart
+++ FirstNullSafety.dart
@@ -1,3 +1,21 @@
+int? getDiem(String user_id, int? diem){
+  if (user_id == 'sv_01')
+    return diem;
+  else
+    return 0;
+}
+
+int getDiem2(String user_id, int? diem){
+  if (user_id == 'sv_01' && diem != null)
+    return diem;
+  else
+    return 0;
+}
+
+int getDiem3(String user_id, int? diem){
+    return diem!;
+}
+
 void main(){
   int? a;
   a = null;
@@ -7,4 +25,11 @@ void main(){
   String? mess;
   mess = "Thong bao";
   print ('a = $a. \n $mess');
+  var core = getDiem('sv_01', 10);
+  print (core);
+  print ('Diem so la: $core');
+  var core2 = getDiem2('sv_02', null);
+  print (core2);
+  var core3 = getDiem3('sv_01', 100);
+  print (core3);
 }
